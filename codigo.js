57563672d4760f7aa78bef2df567ea0e1ad1a46c
jonharki <jonharki@gmail.com>
2016-09-01 16:02:38 -0700

    tp1

--- codigo.js
+++ codigo.js
@@ -1,1 +1,11 @@
-alert("Un mensaje de prueba");
+
+var numeroA = prompt ('ingrese un valor');
+var numeroB = prompt ('ingrese otro valor');
+var resultadoA = parseInt(numeroA) + parseInt(numeroB);
+var resultadoB = parseInt(numeroA) - parseInt(numeroB);
+var resultadoC = parseInt(numeroA) * parseInt(numeroB);
+var resultadoD = parseInt(numeroA) / parseInt(numeroB);
+alert ('El resultado de la suma es ' +  (resultadoA));
+alert ('El resultado de la resta es ' +  (resultadoB));
+alert ('El resultado de la multiplicacion es ' +  (resultadoC));
+alert ('El resultado de la division es ' +  (resultadoD));
